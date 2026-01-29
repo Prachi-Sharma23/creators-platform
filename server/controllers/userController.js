c29de5dc9674b5898b2825e1500b8f6d60eb924d
Prachi Sharma <ps144012@gmail.com>
2026-01-29 13:57:45 +0530

    Done

---
+++ server/controllers/userController.js
@@ -0,0 +1,36 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const exists = await User.findOne({ email });
+    if (exists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User created",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
